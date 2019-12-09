d7322fe69feffda68b4f6f6a396d8e0c3753d040
Jenny-Armstrong <jenny.armstrong1@nhs.net>
2019-12-09 10:42:00 +0000

    load all data extracts from 17/18 publication

--- 1.Data prep.R
+++ 1.Data prep.R
@@ -23,9 +23,36 @@ lookups <- "/05-Analysts/SG-annual-sc-extract/data-lookups/"
 
 sds_data <- read_xlsx(glue(data_extracts, "SDS_All_Files_20190528.xlsx")) %>% 
               clean_names()
+
 # might need the following too: "SDS TREND 20190415.xlsx"  
 sds_trend <- read_xlsx(glue(data_extracts, "SDS TREND 20190415.xlsx")) %>% 
              clean_names()
+
+# list files available in data_extracts folder
+
+list.files(data_extracts)
+
+# Care home files "CH_files_20190516.xlsx"  "CH_extratable_20190417.xlsx"     
+carehome <- read_xlsx(glue(data_extracts, "CH_files_20190516.xlsx")) %>% 
+            clean_names()
+                                             
+# client files "client_alltables20190524_notonly0s_totalsEstimated_withdisclosure.xlsx" - find non-disclosure version
+clients <- read_xlsx(glue(data_extracts, "client_alltables20190524_notonly0s_totalsEstimated_withdisclosure.xlsx")) %>% 
+  clean_names()
+
+# equipment ("Equip_All_Files_20190429.xlsx"     "Equip_Table1_Trenddata_2015_2017.xlsx"                                 
+equipment <- read_xlsx(glue(data_extracts, "Equip_All_Files_20190429.xlsx")) %>% 
+  clean_names()
+
+# home care files: "HC census week rate.xlsx"         "homecare_alltables201900516_latest.xlsx"                               
+homecare <- read_xlsx(glue(data_extracts, "homecare_alltables201900516_latest.xlsx")) %>% 
+  clean_names()
+
+# iorn "IoRN_20190516.xlsx"                                                    
+iorn <- read_xlsx(glue(data_extracts, "IoRN_20190516.xlsx")) %>% 
+  clean_names()
+
+# meals? - check where this is in dashboard "Meals_All_Files_20190515.xlsx" 
 
 # sds data will be used as a test initially before other data sources are pulled in
 
