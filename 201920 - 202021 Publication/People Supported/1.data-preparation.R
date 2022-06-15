c77b5f354f8f66156827f704862edfa5e178811b
Catherine Holland <75622719+catherineholland1@users.noreply.github.com>
2022-06-15 14:37:02 +0100

    Add files via upload

--- 201920 - 202021 Publication/People Supported/1.data-preparation.R
+++ 201920 - 202021 Publication/People Supported/1.data-preparation.R
@@ -141,8 +141,6 @@ write_csv(duplicates_data_people_supported, glue(shiny_app_data_path, "duplicate
 ##### Format data to be used in shiny app #####
 ###############################################.
 
-### where real data files are not available yet, read in dummy files
-
 
 #### TREND DATA -----
 
@@ -156,19 +154,55 @@ data_people_supported_trend <- data_people_supported_trend %>%
                                           "Dumfries & Galloway" = "Dumfries and Galloway",
                                           "Perth & Kinross" = "Perth and Kinross"),
          sending_location = as.character(sending_location),
-         location_order   = ordered(dplyr::recode(sending_location, "Scotland" = "A",
-                                                  "Scotland (All Areas Submitted)" = "A",
-                                                  "Scotland (Estimated)" = "A")),
+         location_order    = dplyr::recode(sending_location, 
+                                           "All Areas Submitted"            = 1,
+                                           "Scotland (All Areas Submitted)" = 1,
+                                           "Scotland"                       = 1,
+                                           "Scotland (Estimated)"           = 1,
+                                           "Aberdeen City"                  = 2,
+                                           "Aberdeenshire"                  = 3,
+                                           "Angus"                          = 4,
+                                           "Argyll and Bute"                = 5,
+                                           "City of Edinburgh"              = 6,
+                                           "Clackmannanshire"               = 7,
+                                           "Comhairle nan Eilean Siar"      = 8,
+                                           "Dumfries and Galloway"          = 9,
+                                           "Dundee City"                    = 10,
+                                           "East Ayrshire"                  = 11,
+                                           "East Dunbartonshire"            = 12,
+                                           "East Lothian"                   = 13,
+                                           "East Renfrewshire"              = 14,
+                                           "Falkirk"                        = 15,
+                                           "Fife"                           = 16,
+                                           "Glasgow City"                   = 17,
+                                           "Highland"                       = 18,
+                                           "Inverclyde"                     = 19,
+                                           "Midlothian"                     = 20,
+                                           "Moray"                          = 21,
+                                           "North Ayrshire"                 = 22,
+                                           "North Lanarkshire"              = 23,
+                                           "Orkney Islands"                 = 24,
+                                           "Perth and Kinross"              = 25,
+                                           "Renfrewshire"                   = 26,
+                                           "Scottish Borders"               = 27,
+                                           "Shetland Islands"               = 28,
+                                           "South Ayrshire"                 = 29,
+                                           "South Lanarkshire"              = 30,
+                                           "Stirling"                       = 31,
+                                           "West Dunbartonshire"            = 32,
+                                           "West Lothian"                   = 33),
          measure          = dplyr::recode(measure,
                                           "Rate per 1,000 population" = "Rate per 1,000 Population",
                                           "Rate per 1,000 population adjusted" = "Rate per 1,000 Population (adjusted)"),
          measure          = as.character(measure),
          nclient          = as.numeric(nclient),
          rate             = as.numeric(round(rate,1)),
-         scotland_nclient = as.numeric(scot_nclient),
-         scotland_rate    = as.numeric(scotland_rate)) %>% 
+         scotland_nclient = as.numeric(scotland_nclient),
+         scotland_rate    = as.numeric(round(scotland_rate, 1))
+         ) %>% 
   arrange(financial_year, location_order,  sending_location, measure)  %>% 
-  select(financial_year, sending_location, measure, nclient, rate, scotland_nclient, scotland_rate)
+  select(financial_year, sending_location, measure, nclient, rate, scotland_rate, scotland_nclient)
+  
 
 
 #### SUMMARY DATA  -----
@@ -183,10 +217,43 @@ data_people_supported_trend <- data_people_supported_trend %>%
                                            "Dumfries & Galloway" = "Dumfries and Galloway",
                                            "Perth & Kinross" = "Perth and Kinross"),
           sending_location = as.character(sending_location),
-          location_order   = ordered(dplyr::recode(sending_location, 
-                                                   "Scotland" = "A",
-                                                   "Scotland (All Areas Submitted)" = "A",
-                                                   "Scotland (Estimated)" = "A")),
+          location_order    = dplyr::recode(sending_location, 
+                                            "All Areas Submitted"            = 1,
+                                            "Scotland (All Areas Submitted)" = 1,
+                                            "Scotland"                       = 1,
+                                            "Scotland (Estimated)"           = 1,
+                                            "Aberdeen City"                  = 2,
+                                            "Aberdeenshire"                  = 3,
+                                            "Angus"                          = 4,
+                                            "Argyll and Bute"                = 5,
+                                            "City of Edinburgh"              = 6,
+                                            "Clackmannanshire"               = 7,
+                                            "Comhairle nan Eilean Siar"      = 8,
+                                            "Dumfries and Galloway"          = 9,
+                                            "Dundee City"                    = 10,
+                                            "East Ayrshire"                  = 11,
+                                            "East Dunbartonshire"            = 12,
+                                            "East Lothian"                   = 13,
+                                            "East Renfrewshire"              = 14,
+                                            "Falkirk"                        = 15,
+                                            "Fife"                           = 16,
+                                            "Glasgow City"                   = 17,
+                                            "Highland"                       = 18,
+                                            "Inverclyde"                     = 19,
+                                            "Midlothian"                     = 20,
+                                            "Moray"                          = 21,
+                                            "North Ayrshire"                 = 22,
+                                            "North Lanarkshire"              = 23,
+                                            "Orkney Islands"                 = 24,
+                                            "Perth and Kinross"              = 25,
+                                            "Renfrewshire"                   = 26,
+                                            "Scottish Borders"               = 27,
+                                            "Shetland Islands"               = 28,
+                                            "South Ayrshire"                 = 29,
+                                            "South Lanarkshire"              = 30,
+                                            "Stirling"                       = 31,
+                                            "West Dunbartonshire"            = 32,
+                                            "West Lothian"                   = 33),
           measure          = dplyr::recode(measure,
                                            "Rate per 1,000 population" = "Rate per 1,000 Population",
                                            "Rate per 1,000 population adjusted" = "Rate per 1,000 Population (adjusted)"),
@@ -194,9 +261,9 @@ data_people_supported_trend <- data_people_supported_trend %>%
           rate             = as.numeric(round(rate, 1)),
           nclient          = as.numeric(nclient),
           scotland_rate    = as.numeric(round(scotland_rate, 1)),
-          scot_nclient     = as.numeric(scot_nclient)) %>% 
+          scotland_nclient     = as.numeric(scotland_nclient)) %>% 
    arrange(financial_year, location_order,  sending_location, measure)  %>% 
-   select(financial_year, sending_location, measure, rate, nclient, scot_nclient, scotland_rate)
+   select(financial_year, sending_location, measure, rate, nclient, scotland_rate, scotland_nclient)
 
 
 #### AGE & SEX  ----
@@ -272,7 +339,8 @@ data_people_supported_age_sex <- data_people_supported_age_sex %>%
                                                "male" = "Male",
                                                "Unknown/N" = "Not Known / Unspecified"),
          sex              = as.character(sex),
-         sex_order        = dplyr::recode(sex, "Female" = 1,
+         sex_order        = dplyr::recode(sex, 
+                                          "Female" = 1,
                                           "Male" = 2,
                                           "Unknown/Not Specified" = 3),
          measure          = dplyr::recode(measure,
@@ -282,7 +350,7 @@ data_people_supported_age_sex <- data_people_supported_age_sex %>%
          nclient          = as.numeric(round(nclient,0)),
          rate             = as.numeric(round(rate,1))) %>% 
   arrange(location_order, sending_location, financial_year, age_order, age_group, sex_order, sex, measure) %>% 
-  select(financial_year, sending_location, age_group, sex, measure, nclient, rate)
+  select(financial_year, sending_location, age_group, sex, measure, rate, nclient)
 
 
 
@@ -365,7 +433,7 @@ data_people_supported_ethnicity <-
                                                  "Caribbean or Black"                  = 1,
                                                  "Any mixed or multiple ethnic groups" = 1,
                                                  "Other Ethnic Group"                  = 2,
-                                                 "Not Provided / Not Known"              = 3),
+                                                 "Not Provided / Not Known"            = 3),
                 measure          = dplyr::recode(measure,
                                                  "Rate per 1,000" = "Rate per 1,000 Social Care Clients",
                                                  "Rate per 1,000 adjusted" = "Rate per 1,000 Social Care Clients (adjusted)"),
@@ -373,7 +441,7 @@ data_people_supported_ethnicity <-
          nclient          = as.numeric(nclient),
          rate             = as.numeric(round(rate,1))) %>% 
   arrange(location_order, sending_location, financial_year, age_order, age_group, ethnicity_order, ethnicity,  measure) %>% 
-  select(financial_year, sending_location, age_group, ethnicity, measure, nclient, rate)
+  select(financial_year, sending_location, age_group, ethnicity, measure, rate, nclient)
 
 
 #### CLIENT GROUP ---- 
@@ -464,7 +532,7 @@ data_people_supported_client_group <- data_people_supported_client_group %>%
          nclient          = as.numeric(nclient),
          rate             = as.numeric(round(rate, 1))) %>% 
   arrange(location_order, sending_location, financial_year, age_order, age_group, client_group_order, client_group, measure) %>% 
-  select(financial_year, sending_location, age_group, client_group, measure, denominator, nclient, rate)
+  select(financial_year, sending_location, age_group, client_group, measure, rate, nclient)
 
 
 
@@ -521,13 +589,14 @@ data_people_supported_services <- data_people_supported_services %>%
                                             "Stirling"                       = 31,
                                             "West Dunbartonshire"            = 32,
                                             "West Lothian"                   = 33),
-         service_group    = dplyr::recode(service, 'HC' = "Home Care",
-                                                          'HS' = "Housing Support",
-                                                          'CH' = "Care Home",
-                                                          'SW' = "Social Worker",
-                                                          'EQ' = "Community Alarms / Telecare",
-                                                          'DC' = "Day Care",
-                                                          'MEALS' = "Meals"),
+         service_group    = dplyr::recode(service, 
+                                          'HC' = "Home Care",
+                                          'HS' = "Housing Support",
+                                          'CH' = "Care Home",
+                                          'SW' = "Social Worker",
+                                          'EQ' = "Community Alarms / Telecare",
+                                          'DC' = "Day Care",
+                                          'MEALS' = "Meals"),
          age_group        = dplyr::recode(age_group, 
                                           '0-17' = "0-17 years",
                                           '18-64' = "18-64 years",
@@ -624,7 +693,7 @@ data_people_supported_meals <- data_people_supported_meals %>%
          nclient          = as.numeric(nclient),
          rate           = as.numeric(round(rate,1))) %>% 
    arrange(location_order, sending_location, financial_year, age_order, age_group, measure) %>% 
-   select(financial_year, sending_location, age_group, measure, nclient, rate)
+   select(financial_year, sending_location, age_group, measure, rate, nclient)
 
 #### LIVING ALONE  ------
 
@@ -714,8 +783,7 @@ data_people_supported_living_alone <-
   filter(
     nclient != "0" & percentage != "0") %>%
   arrange(financial_year, location_order, sending_location, age_order, age_group, living_alone_order, measure) %>% 
-  select(financial_year, sending_location, age_group, living_alone, measure, 
-         nclient, percentage)
+  select(financial_year, sending_location, age_group, living_alone, measure, percentage, nclient)
 
 
 #############################################
